cc034520ade696da0430c657df00e0abb3b67827
Eric <ericmuchiri02@gmail.com>
2019-12-07 15:51:08 +0300

    getting string length

--- 02-strings/strings.sh
+++ 02-strings/strings.sh
@@ -9,3 +9,13 @@ str1='hello'
 str2='there'
 mystring="$str1 $str2"
 echo $mystring
+
+#getting the length of a string
+str='hello this is a bash string'
+echo ${#str}
+
+#extract a substring
+#string:start:length strat(extraction begin) length(extraction size)
+echo ${str:0:5}
+#the output is hello
+
