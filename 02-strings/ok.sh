c917c6cc79833342b05409279ce03bff8ef1625d
Eric <ericmuchiri02@gmail.com>
2019-12-13 13:21:49 +0300

    shell parameters

---
+++ 02-strings/ok.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#shell parameters
+#"n" representa a number
+#$0 represents the file name
+#$1 represents the first parameter
+#$2 represents the second parameter
+#$3 represents the third parameter
+echo "my file name is $0"
+echo "first parameter is $1"
+echo "second parameter is $2"
+echo "Third parameter is $3"
+
+#execute bash ok.sh passing some parameters e.g
+#bash ok.sh me you them
